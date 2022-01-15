cd160285c081b0dadc017964fab2fed2cccbb17f
Victor Ilichev <developer.2020.xx@gmail.com>
2022-01-15 10:38:34 +0300

    added inject random int bean post processor

--- src/main/java/annotation/InjectRandomIntAnnotationBeanPostProcessor.java
+++ src/main/java/annotation/InjectRandomIntAnnotationBeanPostProcessor.java
@@ -2,10 +2,39 @@ package annotation;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.Random;
 
 public class InjectRandomIntAnnotationBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        Field[] fields = bean.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            InjectRandomInt annotation = field.getAnnotation(InjectRandomInt.class);
+
+            // значит аннотация над полем стояла
+            if (annotation != null) {
+                int min = annotation.min();
+                int max = annotation.max();
+                Random random = new Random();
+                int result = min + random.nextInt(max - min);
+
+                // поскольку поле скорее всего в private
+                field.setAccessible(true);
+
+                // можно сделать так, но придется обрабатывать exception
+                // field.set(result);
+                // и кидать exception мы тоже не можем по контракту BeanPostProcessor
+
+                ReflectionUtils.setField(field, bean, result);
+                // теперь прописываем в context.xml
+                // чтобы о нашемпост процессоре узнал спринг
+                // указываем его как обычный бин
+                // ид можно неуказыват ьтак как пользоваться мы этим бином явно не станем
+            }
+        }
         return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
     }
 
--- src/main/java/quoter/TerminatorQuoter.java
+++ src/main/java/quoter/TerminatorQuoter.java
@@ -1,7 +1,28 @@
 package quoter;
 
+import annotation.InjectRandomInt;
+
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
+
+    @InjectRandomInt(min=2, max=7)
+    private int repeat;
+
     private String message;
+
+    @PostConstruct
+    public void init() {
+        // поставили аннотацию @PostConstruct
+        // но она работать не будет)
+        // надо добавить зависимость в проект в файл pom
+        // и прописать бин <bean class="org.springframework.context.annotation.CommonAnnotationBeanPostProcessor" />
+        System.out.println("PHASE 2 repeat: " + this.repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("PHASE 1 repeat: " + this.repeat);
+    }
 
     /**
      * сеттер нужен только для xml конфигов
@@ -13,6 +34,8 @@ public class TerminatorQuoter implements Quoter {
 
     @Override
     public void sayQuote() {
-        System.out.println("message = " + this.message);
+        for (int i = 0; i < this.repeat; i++) {
+            System.out.println("message = " + this.message);
+        }
     }
 }
